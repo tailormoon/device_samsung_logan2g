bd84e99182e088bb9054a60ee42984a18912d5f6
Anil Singh <canilsingh07@gmail.com>
2015-09-21 20:05:11 +0530

    Fix: Storage, Camera and other features

--- cm.mk
+++ cm.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/samsung/logan2g/full_logan2g.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=logan2gxx \
     TARGET_DEVICE=logan2g \
-    BUILD_FINGERPRINT=samsung/logan2gxx/logan2g:4.1.2/JZO54K/S7262XXUAOB1:user/release-keys \
+    BUILD_FINGERPRINT="samsung/logan2gxx/logan2g:4.1.2/JZO54K/S7262XXUAOB1:user/release-keys" \
     PRIVATE_BUILD_DESC="logan2gxx-user 4.1.2 JZO54K S7262XXUAOB1 release-keys"
 
 ## Device identifier. This must come after all inclusions
